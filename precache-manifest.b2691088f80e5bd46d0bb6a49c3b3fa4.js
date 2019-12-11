4ffaabfba664be928b29a4a0309f7146a0a29408
pornpol <pornpol.w@gmail.com>
2019-12-11 14:20:58 +0700

    Updates

---
+++ precache-manifest.b2691088f80e5bd46d0bb6a49c3b3fa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527018be2b81dbcf6b893c01d0b214e9",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "809796a7ca9f4ed71602",
+    "url": "/sudoku/static/css/main.e0c180e5.chunk.css"
+  },
+  {
+    "revision": "a8339f7f0167d185b499",
+    "url": "/sudoku/static/js/2.3a8b223c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sudoku/static/js/2.3a8b223c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "809796a7ca9f4ed71602",
+    "url": "/sudoku/static/js/main.e53773e0.chunk.js"
+  },
+  {
+    "revision": "3e56837d8f07733c6c30",
+    "url": "/sudoku/static/js/runtime-main.d673c4f7.js"
+  }
+]);
